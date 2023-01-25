3177eca5a82d83e3dcbd31821baf8f1580a87bff
alexander <alexander.berzejtis@gmail.com>
2023-01-26 00:12:54 +0300

    Добавлен проект pyCharmProj

---
+++ pyCharmProj/main.py
@@ -0,0 +1,25 @@
+
+# ЗАДАНИЕ 1
+a = "Python is the best programming language in the world"
+b = len(a)
+c = a[6: (b - 7)]
+# результат
+print(c)
+
+# ЗАДАНИЕ 2
+a = "Guido van Rossum is the benevolent dictator for life"
+b = a[::3]
+# результат
+print(b)
+
+# ЗАДАНИЕ 4
+a = "You have a problem with authority, Mr. Anderson."
+symb = set(a) #разбиваем строку на отдельные символы без повторов
+print(symb)
+c = list(map(a.count, symb)) #ищем кол-во повторений для каждого элемента из "symb" в строке "a"
+print(c)
+d = list(zip(symb, c)) #создаем список из пар (символ, кол-во ссылок)
+data = dict(d) #создаем словарь
+
+# результат
+print(data)
